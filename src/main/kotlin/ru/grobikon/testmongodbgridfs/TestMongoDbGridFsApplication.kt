607dc00c4877f22614a692998a1e19929cec07dd
Образцов Григорий <grobikon@mail.ru>
2021-12-05 01:53:03 +0300

    Added project.

---
+++ src/main/kotlin/ru/grobikon/testmongodbgridfs/TestMongoDbGridFsApplication.kt
@@ -0,0 +1,11 @@
+package ru.grobikon.testmongodbgridfs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestMongoDbGridFsApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestMongoDbGridFsApplication>(*args)
+}
